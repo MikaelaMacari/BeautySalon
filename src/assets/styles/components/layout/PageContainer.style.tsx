158c135b125d72ea9e93447316aed1a54c502650
Mihaela Macari <mikaela.malanici@gmail.com>
2022-09-09 09:38:38 +0300

    Added home content and styles

--- src/assets/styles/components/layout/PageContainer.style.tsx
+++ src/assets/styles/components/layout/PageContainer.style.tsx
@@ -1,8 +1,9 @@
 import styled from "styled-components";
 import { colors } from "../../variables.style";
 const Container = styled.div`
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
+  min-height: 100vh;
+  padding-bottom: 30px;
   background-color: ${colors.primaryBgColor};
 `;
 export default Container;
