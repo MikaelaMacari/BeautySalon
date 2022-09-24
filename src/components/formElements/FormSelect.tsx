668c764ae4dc22e1ccc3681f77920da1aa534ba1
Tudor Teisanu <tudor.teisanu@powerit.dev>
2022-09-24 17:29:41 +0300

    fixed form select input value

--- src/components/formElements/FormSelect.tsx
+++ src/components/formElements/FormSelect.tsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import Grid from "@mui/material/Grid";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import Label from "./Label";
-import Input from "./Input";
 import Dropdown from "./Dropdown";
 import Error from "./Error";
 
@@ -20,31 +19,51 @@ interface FormSelectInterface {
   list?: any;
 }
 const FormSelect = ({ inputName, register, errors, type, placeholder, validationSchema, list, title, description }: FormSelectInterface) => {
-  const [inputValue, setinputValue] = useState<string>("");
+  const [inputValue, setSelected] = useState<Record<string, any>>();
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
   const openDropdown = () => {
     setIsOpen((currentState) => !currentState);
   };
+
   const handleClick = (value: any) => {
     openDropdown();
-    setinputValue(value.name);
+    setSelected(value);
   };
+
+  const getValue = () => {
+    if (inputValue) {
+      return inputValue.id
+    }
+
+    return null
+  }
+
+  const getText = () => {
+    if (inputValue) {
+      return inputValue.name
+    }
+
+    return placeholder
+  }
   return (
     <Grid container spacing={3}>
       <Grid item xs={12} sm={2} md={1} lg={2}>
         <Label title={title} description={description} />
       </Grid>
       <Grid item xs={10} sm={8} md={10} lg={8}>
-        <Input
+        <div
+          onClick={() => openDropdown()}
+          style={{
+          width: "100%",
+          height: "100%"
+        }}>{getText()}</div>
+        <input
+          style={{display: 'none'}}
           id={inputName}
-          inputName={inputName}
-          type={type}
-          placeholder={placeholder}
-          register={register}
-          validationSchema={validationSchema}
-          openDropdown={openDropdown}
-          value={inputValue}
+          name={inputName}
+          value={getValue()}
+          readOnly
         />
         {errors && <Error errorMessage={errors[inputName]?.message} />}
         {isOpen && <Dropdown list={list} handleClick={handleClick} />}
